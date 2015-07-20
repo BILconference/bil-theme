823ca2990dddd36603f6ff893ef5c4872838c93e
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 21:08:49 -0500

    no message

--- front-page.php
+++ front-page.php
@@ -1,24 +1,15 @@
 <?php get_header(); ?>
-
 	<?php
-
 	// check if the repeater field has rows of data
 	if( have_rows('modules') ):
-
 	 	// loop through the rows of data
 	    while ( have_rows('modules') ) : the_row();
-
 	        // display a sub field value
 	        $module = get_sub_field('module');
 	        get_template_part( 'partials/mod', $module->post_name );
-
 	    endwhile;
-
 	else :
-
 	    // no rows found
-
 	endif;
-
 	?>
 <?php get_footer(); ?>

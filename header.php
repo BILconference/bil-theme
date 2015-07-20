5f3d77fe884520792e03a4fa30a0f796fa9ea3b9
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 00:18:28 -0500

    no message

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <!doctype html>
+<?php flush_rewrite_rules(); ?>
+
 
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->

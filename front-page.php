f6f8dc92e71219767b807cdb9c9f7414548e8c48
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 20:37:40 -0500

    no message

--- front-page.php
+++ front-page.php
@@ -1,4 +1,15 @@
 <?php get_header(); ?>
+<?php
+
+if( have_rows('modules') ):
+	while ( have_rows('repeater_field_name') ) : the_row();
+		the_sub_field('sub_field_name');
+	endwhile;
+else:
+	// no rows found
+endif;
+
+?>
 	<div class="container-fluid" id="module-hero">
 		<div class="row">
 			<div class="col-xs-12">
@@ -34,7 +45,7 @@
 	<div class="container-fluid" id="module-cta">
 		<div class="row">
 			<div class="col-xs-12">
-				Sponsors
+				Call to Action
 				This should be full width
 			</div>
 		</div>

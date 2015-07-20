e477147d6c939af9a7bb96d0b93e8b3433a0415a
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 22:23:25 -0500

    no message

--- partials/mod-hero.php
+++ partials/mod-hero.php
@@ -3,10 +3,12 @@
 <?php $image = $background['sizes'][ 'full-width' ]; ?>
 <?php $text = get_field("text", $module->ID, OBJECT); ?>
 
-<div class="container-fluid" id="module-hero" style="background-image: url('<?php echo $image; ?>');">
-	<div class="row">
-		<div class="col-xs-12">
-			<?php echo $text ?>
+<div id="module-hero" style="background-image: url('<?php echo $image; ?>');" >
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<?php echo $text ?>
+			</div>
 		</div>
 	</div>
 </div>
--- partials/mod-sponsors.php
+++ partials/mod-sponsors.php
@@ -1,10 +1,11 @@
 <?php $module = get_module_by_slug('sponsors') ?>
-
-<div class="container-fluid" id="module-sponsors">
-	<div class="row">
-		<div class="col-xs-12">
-			Sponsors
-			This should be full width
+<div id="module-sponsors">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+				Sponsors
+				This should be full width
+			</div>
 		</div>
 	</div>
 </div>

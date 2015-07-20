0fa9e42cfc3085d5f2f80c6dc6b2789adadf8012
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 15:21:40 -0500

    New image size (200x200) and hooked it up to Speakers Module.

--- partials/mod-speakers.php
+++ partials/mod-speakers.php
@@ -4,20 +4,19 @@
 <div id="module-speakers">
 	<div class="container">
 		<div class="row">
-			<?php if ( $speakers->have_posts() ) : ?>
-				<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
-					<div class="col-xs-12 col-sm-6 col-md-2">
-						<?php $background = get_field('speaker_photo', $post->ID, OBJECT); ?>
-						<?php $image = $background['sizes'][ '300x100' ]; ?>
-
-						<div class="tile">
-							<img src="<?php echo $image; ?>">
-							<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+			<div class="col-xs-12">
+				<?php if ( $speakers->have_posts() ) : ?>
+					<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
+						<div class="col-xs-12 col-sm-6 col-md-2">
+							<div class="tile">
+								<?php the_post_thumbnail( '200x200', $attr ); ?>
+								<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+							</div>
 						</div>
-					</div>
-				<?php endwhile; ?>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
+					<?php endwhile; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>

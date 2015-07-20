f9c565c8a770a763898d71357ed7eef38a1babc0
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 22:06:44 -0500

    no message

--- partials/mod-hero.php
+++ partials/mod-hero.php
@@ -1,10 +1,8 @@
 <?php $module = get_module_by_slug("hero"); ?>
 <?php $background = get_field("background_image", $module->ID, OBJECT); ?>
-<?php var_dump($background); ?>
 <?php $image = $background['sizes'][ 'full-width' ]; ?>
-<?php echo $image; ?>
 
-<div class="container-fluid" id="module-hero" style="background-image: url('');">
+<div class="container-fluid" id="module-hero" style="background-image: url('<?php echo $image');">
 	<div class="row">
 		<div class="col-xs-12">
 			Hero Module

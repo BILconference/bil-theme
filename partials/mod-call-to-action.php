449aa02e1f39bc0cb84ac9d26c534ee5475bbf75
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 01:53:10 -0500

    no message

--- partials/mod-call-to-action.php
+++ partials/mod-call-to-action.php
@@ -4,9 +4,10 @@
 	<div class="container">
 		<div class="row">
 			<div id="join-slack" class="col-xs-12 col-md-6">
-				<script async defer src="https://bil-slack.herokuapp.com/slackin.js?large"></script>
+				Host a BIL
 			</div>
 			<div class="col-xs-12 col-md-6">
+				<script async defer src="https://bil-slack.herokuapp.com/slackin.js?large"></script>
 				<div class="iframe-wrapper">
 					<iframe style="width:100%; height:400px;" src="https://bil-slack.herokuapp.com/" scrolling="no"></iframe>
 				</div>

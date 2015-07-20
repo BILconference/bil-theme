bf17abcb8a5eb51590a8558a63db1f55d949e81d
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 01:40:29 -0500

    no message

--- partials/mod-map.php
+++ partials/mod-map.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-md-8">
-				<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/superphly.mooogbjb/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1Ijoic3VwZXJwaGx5IiwiYSI6IjVlYzljYzk3NDY4YWMwNDA3Zjc0NjdjYWYxOThkOWMyIn0.T7cd_y4tv_N3DoBofUZZqQ'></iframe>
+				<iframe width="100%" height="300px" frameBorder='0' src='https://a.tiles.mapbox.com/v4/superphly.mooogbjb/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1Ijoic3VwZXJwaGx5IiwiYSI6IjVlYzljYzk3NDY4YWMwNDA3Zjc0NjdjYWYxOThkOWMyIn0.T7cd_y4tv_N3DoBofUZZqQ'></iframe>
 			</div>
 			<div class="col-xs-12 col-md-4">
 				List of upcoming events goes here

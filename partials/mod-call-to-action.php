a7f3ba3c2e16cdd47e3f8a0265319b6984a492b0
Michael Cummings <michaeldcummings@gmail.com>
2015-07-20 00:07:31 -0500

    slack

--- partials/mod-call-to-action.php
+++ partials/mod-call-to-action.php
@@ -7,8 +7,9 @@
 				<script async defer src="https://bil-slack.herokuapp.com/slackin.js?large"></script>
 			</div>
 			<div class="col-xs-12 col-md-6">
-				<iframe style="width:100%; height:400px;" src="https://bil-slack.herokuapp.com/">
-				</iframe>
+				<div class="iframe-wrapper">
+					<iframe style="width:100%; height:400px;" src="https://bil-slack.herokuapp.com/" scrolling="no"></iframe>
+				</div>
 			</div>
 		</div>
 	</div>

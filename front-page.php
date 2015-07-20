1d14064b9f990452b6a11685e35d45946e782fdc
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 19:45:03 -0500

    no message

--- front-page.php
+++ front-page.php
@@ -1,35 +1,37 @@
-		<div class="container-fluid" id="module-hero">
-			<div class="row">
-				<div class="col-xs-12">
-					This should be full width
-				</div>
+<?php get_header(); ?>
+	<div class="container-fluid" id="module-hero">
+		<div class="row">
+			<div class="col-xs-12">
+				This should be full width
 			</div>
 		</div>
-		<div class="container-fluid" id="module-map">
-			<div class="row">
-				<div class="col-xs-12">
-					This should be full width
-				</div>
+	</div>
+	<div class="container-fluid" id="module-map">
+		<div class="row">
+			<div class="col-xs-12">
+				This should be full width
 			</div>
 		</div>
-		<div class="container-fluid" id="module-intro">
-			<div class="row">
-				<div class="col-xs-12">
-					This should be full width
-				</div>
+	</div>
+	<div class="container-fluid" id="module-intro">
+		<div class="row">
+			<div class="col-xs-12">
+				This should be full width
 			</div>
 		</div>
-		<div class="container-fluid" id="module-sponsors">
-			<div class="row">
-				<div class="col-xs-12">
-					This should be full width
-				</div>
+	</div>
+	<div class="container-fluid" id="module-sponsors">
+		<div class="row">
+			<div class="col-xs-12">
+				This should be full width
 			</div>
 		</div>
-		<div class="container-fluid" id="module-cta">
-			<div class="row">
-				<div class="col-xs-12">
-					This should be full width
-				</div>
+	</div>
+	<div class="container-fluid" id="module-cta">
+		<div class="row">
+			<div class="col-xs-12">
+				This should be full width
 			</div>
 		</div>
+	</div>
+<?php get_footer(); ?>

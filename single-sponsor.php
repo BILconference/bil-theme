394122d21ff1bd5ea83b60ef244dc4bece2143a9
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 00:28:52 -0500

    no message

--- single-sponsor.php
+++ single-sponsor.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<h1><?php the_title(); ?></h1>
+			<h1>Supporter: <?php the_title(); ?></h1>
 			<?php the_field('about_long'); ?>
 		</div>
 	</div>

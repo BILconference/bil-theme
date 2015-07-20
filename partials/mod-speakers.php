b6f3f29bda2e9ae8b551e2820569c03b3e26d374
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 03:20:37 -0500

    no message

--- partials/mod-speakers.php
+++ partials/mod-speakers.php
@@ -1,10 +1,11 @@
 <?php $module = get_module_by_slug('speakers') ?>
-<?php $speakers = new WP_Query( array('post_type' => 'speakers', 'orderby' => 'menu_order', 'posts_per_page' => -1) ); ?>
+<?php $speakers = new WP_Query( array('post_type' => 'speaker', 'orderby' => 'menu_order', 'posts_per_page' => -1, 'category_name' => 'featured') ); ?>
 
-<?php if ( $speakers->have_posts() ) : ?>
+
 <div id="module-speakers">
 	<div class="container">
 		<div class="row">
+			<?php if ( $speakers->have_posts() ) : ?>
 				<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
 					<div class="col-xs-12 col-sm-6 col-md-2">
 						<?php $background = get_field('speaker_photo', $post->ID, OBJECT); ?>
@@ -14,11 +15,10 @@
 							<img src="<?php echo $image; ?>">
 							<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
 						</div>
-					
 					</div>
 				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
-<?php wp_reset_postdata(); ?>
-<?php endif; ?>

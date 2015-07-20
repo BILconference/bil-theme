306c6128025568791090724faae9453e07591336
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 16:18:57 -0500

    no message

--- single-event.php
+++ single-event.php
@@ -58,77 +58,66 @@
 
 
 
-					<?php if (get_field('iframe')) { ?>
-						<iframe seamless="seamless" border="0" height="1000" src="<?php the_field('iframe'); ?>" width="100%">
-							<p>Your browser does not support iframes.</p>
-						</iframe>
-					<?php } else { ?>
-						<div class="event-info">
-							<div class="cf">
-								<ul class="date-contact">
-									<li>
-										<span>Start Date:</span>
-										<?php if (get_field('start_date')) { 
-											$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
-											echo $start_date->format('F jS, Y');
-										} ?>
-									</li>
-									<li>
-										<span>End Date:</span>
-										<?php if (get_field('end_date')) { 
-											$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
-											echo $end_date->format('F jS, Y');
-										} ?>
-									</li>
-									<li>
-										<span>Contact:</span>
-										<a href="mailto:<?php the_field('organizer_email'); ?>"><?php the_field('organizer_name'); ?></a>
-									</li>
-								</ul>
-								<ul class="location cf">
-									<li>
-										<span>Location:</span>
-										<?php the_field('general_location'); ?>
-									</li>
-									<li>
-										<span>Address:</span>
-										<address><?php the_field('specific_location'); ?></address>
-									</li>
-								</ul>
-							</div>
-							<p class="cf">
-								<span>Social:</span>
-								<?php if (get_field('facebook_event')) { ?>
-									<a href="<?php the_field('facebook_event'); ?>">Facebook Event</a>
-								<?php } ?>
-								<?php if (get_field('facebook_page')) { ?>
-									| <a href="<?php the_field('facebook_page'); ?>">Facebook Page</a>
-								<?php } ?>
-								<?php if (get_field('twitter')) { ?>
-									| <a href="<?php the_field('twitter'); ?>">Twitter</a>
-								<?php } ?>
-								<?php if (get_field('ticketing')) { ?>
-									<a href="<?php the_field('ticketing'); ?>">Tickets</a>
-								<?php } ?>
-							</p>
-						</div>
+	<?php if (get_field('iframe')) { ?>
+		<iframe seamless="seamless" border="0" height="1000" src="<?php the_field('iframe'); ?>" width="100%">
+			<p>Your browser does not support iframes.</p>
+		</iframe>
+	<?php } else { ?>
+		<div class="event-info">
+			<div class="cf">
+				<ul class="date-contact">
+					<li>
+						<span>Start Date:</span>
+						<?php if (get_field('start_date')) { 
+							$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
+							echo $start_date->format('F jS, Y');
+						} ?>
+					</li>
+					<li>
+						<span>End Date:</span>
+						<?php if (get_field('end_date')) { 
+							$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
+							echo $end_date->format('F jS, Y');
+						} ?>
+					</li>
+					<li>
+						<span>Contact:</span>
+						<a href="mailto:<?php the_field('organizer_email'); ?>"><?php the_field('organizer_name'); ?></a>
+					</li>
+				</ul>
+				<ul class="location cf">
+					<li>
+						<span>Location:</span>
+						<?php the_field('general_location'); ?>
+					</li>
+					<li>
+						<span>Address:</span>
+						<address><?php the_field('specific_location'); ?></address>
+					</li>
+				</ul>
+			</div>
+			<p class="cf">
+				<span>Social:</span>
+				<?php if (get_field('facebook_event')) { ?>
+					<a href="<?php the_field('facebook_event'); ?>">Facebook Event</a>
+				<?php } ?>
+				<?php if (get_field('facebook_page')) { ?>
+					| <a href="<?php the_field('facebook_page'); ?>">Facebook Page</a>
+				<?php } ?>
+				<?php if (get_field('twitter')) { ?>
+					| <a href="<?php the_field('twitter'); ?>">Twitter</a>
+				<?php } ?>
+				<?php if (get_field('ticketing')) { ?>
+					<a href="<?php the_field('ticketing'); ?>">Tickets</a>
+				<?php } ?>
+			</p>
+		</div>
 
-						<?php if (get_field('ticketing_embed_code')) { ?>
-							<h2>Ticketing</h2>
-							<?php $embed = get_field('ticketing_embed_code'); ?>
-							<?php echo sprintf($embed); ?>
-						<?php } ?>
+		<?php if (get_field('ticketing_embed_code')) { ?>
+			<h2>Ticketing</h2>
+			<?php $embed = get_field('ticketing_embed_code'); ?>
+			<?php echo sprintf($embed); ?>
+		<?php } ?>
+	<? } ?>
 
-						<div class="content">
-							<?php the_content(); ?>
-						</div>
-					<?php } ?>
-				</section>
-				<footer class="article-footer">
-      				<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-				</footer>
-			</article>
-		<?php endwhile; endif; ?>
-	</div>
-</div>
 <?php get_footer(); ?>

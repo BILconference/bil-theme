8d90c61f6aa6fa53fcad3ed2999aa8432188cf8b
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 18:49:10 -0500

    no message

--- single-event.php
+++ single-event.php
@@ -12,60 +12,85 @@
 
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-
-<div id="event-hero" style="background-image: url('<?php echo $image; ?>');" >
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
-				<?php the_field('general_location'); ?><br>
-				<?php if (get_field('start_date')) { 
-					$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
-					echo $start_date->format('F jS, Y');
-				} ?>
-				<?php if (get_field('end_date')) { 
-					echo " &#9658; ";
-					$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
-					echo $end_date->format('F jS, Y');
-				} ?>
+	<div id="event-hero" style="background-image: url('<?php echo $image; ?>');" >
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1><?php the_title(); ?></h1>
+					<?php the_field('general_location'); ?><br>
+					<?php if (get_field('start_date')) { 
+						$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
+						echo $start_date->format('F jS, Y');
+					} ?>
+					<?php if (get_field('end_date')) { 
+						echo " &#9658; ";
+						$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
+						echo $end_date->format('F jS, Y');
+					} ?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<div id="event-about" style="background-image: url('<?php echo $image; ?>');" >
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-md-8 content">
-				
-			</div>
-			<div class="col-xs-12 col-md-4">
-				<h2>Organizers</h2>
-				<ul>
-					<li>Cody Marx Bailey</li>
-					<li>Michael Cummings</li>
-					<li>Ryan Plesko</li>
-					<li>Brad Shende</li>
-					<li>Jackson Smith</li>
-				</ul>
+	<div id="event-about">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-8 content">
+					<?php the_content(); ?>
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<h2>Organizers</h2>
+					<ul>
+						<li>Cody Marx Bailey</li>
+						<li>Michael Cummings</li>
+						<li>Ryan Plesko</li>
+						<li>Brad Shende</li>
+						<li>Jackson Smith</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+
+	<div id="event-talks">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="event-sponsors">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					Sponsors
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="event-cta">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					Call to Action
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-
-
-
-	<?php if (get_field('iframe')) { ?>
-		<iframe seamless="seamless" border="0" height="1000" src="<?php the_field('iframe'); ?>" width="100%">
-			<p>Your browser does not support iframes.</p>
-		</iframe>
-	<?php } else { ?>
-		<div class="event-info">
-			<div class="cf">
+		<?php if (get_field('iframe')) { ?>
+			<iframe seamless="seamless" border="0" height="1000" src="<?php the_field('iframe'); ?>" width="100%">
+				<p>Your browser does not support iframes.</p>
+			</iframe>
+		<?php } else { ?>
+			<div class="event-info">
 				<ul class="date-contact">
 					<li>
 						<span>Start Date:</span>
@@ -96,8 +121,7 @@
 						<address><?php the_field('specific_location'); ?></address>
 					</li>
 				</ul>
-			</div>
-			<p class="cf">
+
 				<span>Social:</span>
 				<?php if (get_field('facebook_event')) { ?>
 					<a href="<?php the_field('facebook_event'); ?>">Facebook Event</a>
@@ -111,14 +135,14 @@
 				<?php if (get_field('ticketing')) { ?>
 					<a href="<?php the_field('ticketing'); ?>">Tickets</a>
 				<?php } ?>
-			</p>
-		</div>
+			</div>
 
-		<?php if (get_field('ticketing_embed_code')) { ?>
-			<h2>Ticketing</h2>
-			<?php $embed = get_field('ticketing_embed_code'); ?>
-			<?php echo sprintf($embed); ?>
-		<?php } ?>
-	<? } ?>
+			<?php if (get_field('ticketing_embed_code')) { ?>
+				<h2>Ticketing</h2>
+				<?php $embed = get_field('ticketing_embed_code'); ?>
+				<?php echo sprintf($embed); ?>
+			<?php } ?>
+		<? } ?>
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>

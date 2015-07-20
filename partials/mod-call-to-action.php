6df3b6d1b9afb773e2e45984fbbaa60fce69d8d8
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 22:26:42 -0500

    no message

--- partials/mod-call-to-action.php
+++ partials/mod-call-to-action.php
@@ -3,9 +3,11 @@
 <div id="module-cta">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12">
-				Call to Action
-				This should be full width
+			<div class="col-xs-12 col-md-6">
+				Join Slack
+			</div>
+			<div class="col-xs-12 col-md-6">
+				Sign Up
 			</div>
 		</div>
 	</div>

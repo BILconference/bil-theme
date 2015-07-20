8a8b75c90bfb8c85636cfc64ce0dd6cd25d64d57
Cody Marx Bailey <superphly@gmail.com>
2015-07-19 21:59:53 -0500

    no message

--- inc/thumbnail.php
+++ inc/thumbnail.php
@@ -1,7 +1,10 @@
 <?php
 
 // Add Image Sizes
-add_image_size( 'bil-full-width', 1920, 1050, true );
+
+add_image_size( '600x150', 600, 150, true );
+add_image_size( '300x100', 300, 100, true );
+add_image_size( '1920x1050', 1920, 1050, true );
 
 
 // Make the image size slugs, pretty for end users.
@@ -10,7 +13,7 @@ function bones_custom_image_sizes( $sizes ) {
     return array_merge( $sizes, array(
         '600x150' => __('600px by 150px'),
         '300x100' => __('300px by 100px'),
-        '1920x1050' => __('1920px by 1050px'),
+        '1920x1050' => __('1920px by 1050px')
     ) );
 }
 

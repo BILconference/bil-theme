0b0cbba989e0d1b9f0573ccbae4b9ed16665bec6
Michael Cummings <michaeldcummings@gmail.com>
2015-07-19 23:52:50 -0500

    larger slack

--- partials/mod-call-to-action.php
+++ partials/mod-call-to-action.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 			<div id="join-slack" class="col-xs-12 col-md-6">
-				<script async defer src="https://bil-slack.herokuapp.com/slackin.js"></script>
+				<script async defer src="https://bil-slack.herokuapp.com/slackin.js?large"></script>
 			</div>
 			<div class="col-xs-12 col-md-6">
 				Sign Up

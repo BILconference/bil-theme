f158974c5122537d72cdf98315b0dc4739710ce5
Michael Cummings <michaeldcummings@gmail.com>
2015-07-21 12:16:54 -0500

    bg image to intro

--- partials/mod-intro.php
+++ partials/mod-intro.php
@@ -1,7 +1,15 @@
 <?php $module = get_module_by_slug('intro') ?>
 <?php $text = get_field("text", $module->ID, OBJECT); ?>
 
-<div id="module-intro">
+
+<?php if (has_post_thumbnail( $post->ID ) ): 
+	$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full-width' );
+endif; ?>
+
+
+
+
+<div id="module-intro" style="background-image: url('<?php echo $image[0]; ?>')">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 borders">

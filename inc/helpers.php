a5edc88a882c08a418de5265eefe5543cc20b181
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 02:01:55 -0500

    no message

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,4 +1,7 @@
 <?php
+
+// Quick shortcut to get the right module.
+
 function get_module_by_slug($page_slug) {
 	$module = get_page_by_path($page_slug, OBJECT, 'module');
 	if ($module) {
@@ -6,4 +9,50 @@ function get_module_by_slug($page_slug) {
 	} else {
 		return null;
 	}
+}
+
+// Gross code to go grab the upcoming events and pass them back as an array of objects for you to loop over
+
+function upcoming_bils() {
+	$today = date('Ymd');
+
+    $args = array (
+		'posts_per_page' => -1,
+		'post_type'     => 'event',
+		'meta_query'    => array(
+			'key'   => 'end_date',
+			'compare' => '>=',
+			'value'   => $today
+		),
+		'orderby'     => 'meta_value_num',
+		'meta_key'      => 'end_date',
+		'order'       => 'ASC'
+	);
+
+	$upcoming_bils = new WP_Query($args);
+
+	return $upcoming_bils;
+}
+
+// Gross code to go grab the past events and pass them back as an array of objects for you to loop over
+
+function past_bils() {
+	$today = date('Ymd');
+
+    $args = array (
+		'posts_per_page' => -1,
+		'post_type'     => 'event',
+		'meta_query'    => array(
+			'key'   => 'end_date',
+			'compare' => '<',
+			'value'   => $today
+		),
+		'orderby'     => 'meta_value_num',
+		'meta_key'      => 'end_date',
+		'order'       => 'ASC'
+	);
+
+	$past_bils = new WP_Query($args);
+
+	return $past_bils;
 }

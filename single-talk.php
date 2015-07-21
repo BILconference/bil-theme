9d8cc371bc2657aecaad73fa0e7943e84e35ca4b
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 03:08:28 -0500

    no message

--- single-talk.php
+++ single-talk.php
@@ -7,21 +7,25 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-
-					<?php $event = get_field('event'); ?>
-					<?php if( $event ) { ?>
-						<a href="<?php echo get_permalink( $event->ID ); ?>">
-							<?php echo get_the_title( $event->ID ); ?>
-						</a>
-					<?php } ?>
 
 					<?php $speaker = get_field('speaker'); ?>
-					<?php if( $speaker ) { ?>
-						<a href="<?php echo get_permalink( $speaker->ID ); ?>">
-							<?php echo get_the_title( $speaker->ID ); ?>
-						</a>
-					<?php } ?>
+					<?php $event = get_field('event'); ?>
+
+					<p>
+						<?php if( $speaker ) { ?>
+							<a href="<?php echo get_permalink( $speaker->ID ); ?>">
+								<?php echo get_the_title( $speaker->ID ); ?>
+							</a>
+						<?php } else { ?> unknown <?php } ?>
+						at
+						<?php if( $event ) { ?>
+							<a href="<?php echo get_permalink( $event->ID ); ?>">
+								<?php echo get_the_title( $event->ID ); ?>
+							</a>
+						<?php } else { ?> unknown <?php } ?>
+					</p>
+
+					<?php the_content(); ?>
 				</div>
 			</div>
 		</div>

0e7a621b19ac55a68c39dd872e47e4134148e7a7
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 11:59:27 -0500

    no message

--- page.php
+++ page.php
@@ -4,12 +4,12 @@
 	<div id="page-<?php $post->post_name; ?>">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
 					<h1><?php the_title(); ?></h1>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
 					<?php the_content(); ?>
 				</div>
 			</div>

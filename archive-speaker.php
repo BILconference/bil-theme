1a72ffbd9b810c8aad1fc7191ef9545c23389f65
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 22:13:52 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -1,18 +1,12 @@
 <?php get_header(); ?>
 
-	<div id="talk-list">
+	<div id="speaker-list">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<h1><?php the_title(); ?></h1>
-
-						<?php $speaker = get_field('speaker'); ?>
-						<?php if( $speaker ) { ?>
-							<a href="<?php echo get_permalink( $speaker->ID ); ?>">
-								<?php echo get_the_title( $speaker->ID ); ?>
-							</a>
-						<?php } ?>
+						<?php the_thumbnail(); ?>
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

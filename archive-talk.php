981b439e42523339c9ac8c258d6e20293dde61c7
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 00:26:45 -0500

    no message

--- archive-talk.php
+++ archive-talk.php
@@ -10,7 +10,7 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<h1><?php the_title(); ?></h1>
+						<h3><?php the_title(); ?></h3>
 
 						<?php $speaker = get_field('speaker'); ?>
 						<?php if( $speaker ) { ?>
--- inc/cpt-archive.php
+++ inc/cpt-archive.php
@@ -6,3 +6,11 @@ function cpt_archive_speaker($query) {
 }
  
 add_action('pre_get_posts', 'cpt_archive_speaker');
+
+
+function cpt_archive_event($query) {
+    if ($query->is_main_query() && $query->is_post_type_archive('event') && !is_admin())
+        $query->set('posts_per_page', -1);
+}
+ 
+add_action('pre_get_posts', 'cpt_archive_event');

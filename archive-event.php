bfa1434474d42840dfed6ce139153878a6c2bd33
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 00:23:31 -0500

    archive-event.php added. fixed some headings with other archive pages.

--- archive-event.php
+++ archive-event.php
@@ -4,20 +4,13 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
-					<h1>Talks</h1>
+					<h1>Events</h1>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<h1><?php the_title(); ?></h1>
-
-						<?php $speaker = get_field('speaker'); ?>
-						<?php if( $speaker ) { ?>
-							<a href="<?php echo get_permalink( $speaker->ID ); ?>">
-								<?php echo get_the_title( $speaker->ID ); ?>
-							</a>
-						<?php } ?>
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

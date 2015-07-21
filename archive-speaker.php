7bbb697f1930c088da76c5c53139a40dac6ca11b
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 22:28:17 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -5,7 +5,7 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<h1><?php the_title(); ?></h1>
+						<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

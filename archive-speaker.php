19e500c04616b089aa13d38a7d6265a02ab5609f
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 22:21:59 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -6,7 +6,6 @@
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<h1><?php the_title(); ?></h1>
-						<?php the_thumbnail(); ?>
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

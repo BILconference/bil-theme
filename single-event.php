b63cbdaf5a6e3abdde8cab75c3542fcff3bc6254
Cody Marx Bailey <superphly@gmail.com>
2015-07-20 20:00:36 -0500

    no message

--- single-event.php
+++ single-event.php
@@ -65,7 +65,7 @@
 						'meta_query' => array(
 							array(
 								'key' => 'event', // name of custom field
-								'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
+								'value' => '"' . $post->ID . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
 								'compare' => 'LIKE'
 							)
 						)

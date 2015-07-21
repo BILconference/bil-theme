a325c871a83da6ed7f15ee5d219a62f585bb5c60
Michael Cummings <michaeldcummings@gmail.com>
2015-07-21 17:13:33 -0500

    -

--- partials/mod-call-to-action.php
+++ partials/mod-call-to-action.php
@@ -3,10 +3,10 @@
 <div id="module-cta">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-6">
+			<div class="col-xs-12 col-md-9">
 				Host a BIL
 			</div>
-			<div class="col-xs-12 col-md-6">
+			<div class="col-xs-12 col-md-3">
 				<a href="/chat"><img class="img-responsive" src="http://bilconference.com/wp-content/uploads/2015/07/Slack-Join.jpg" alt="Join BIL on Slack" /></a>
 			</div>
 		</div>

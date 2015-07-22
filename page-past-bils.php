b64b15810e95f6c77338f961898154b1b53dd045
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 23:13:26 -0500

    no message

--- page-past-bils.php
+++ page-past-bils.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div class="content">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-md-8">
+<div class="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-8">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<h1><?php the_title(); ?></h1>
 					<?php $past_bils = past_bils(); ?>
 					<?php if ($past_bils->have_posts() ) : ?>
@@ -17,13 +17,13 @@
 						</ul>
 						<?php wp_reset_postdata(); ?>
 					<?php endif; ?>
-				</div>
-				<div class="col-xs-12 col-md-4">
-					sidebar
-				</div>
+				<?php endwhile; endif; ?>
+			</div>
+			<div class="col-xs-12 col-md-4">
+				sidebar
 			</div>
 		</div>
 	</div>
-<?php endwhile; endif; ?>
+</div>
 
 <?php get_footer(); ?>

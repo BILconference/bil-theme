9523c56fe44bca2a87eb4ce46af6bf94f85059f6
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 21:56:39 -0500

    no message

--- partials/mod-speakers.php
+++ partials/mod-speakers.php
@@ -3,23 +3,21 @@
 
 <div id="module-speakers">
 	<div class="container">
-		<div class="row row-eq-height">
-			<?php if ( $speakers->have_posts() ) : ?>
-				<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
-					<?php if (has_post_thumbnail()): ?>
-						<div class="col-xs-12 col-sm-6 col-md-2">
+		<div class="row">
+			<div class="col-xs-12">
+				<?php if ( $speakers->have_posts() ) : ?>
+					<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
+						<?php if (has_post_thumbnail()): ?>
 							<div class="tile">
 								<?php the_post_thumbnail( '200x200', $attr ); ?>
 								<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
 								<h6><?php the_field('association') ?></h6>
 							</div>
-						</div>
-					<?php else :?>
-					<?php endif;?>
-					
-				<?php endwhile; ?>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
+						<?php endif;?>
+					<?php endwhile; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>

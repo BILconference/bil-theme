cac162d5b1be96884537960e1ab2ad370c0d409b
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 22:03:32 -0500

    no message

--- partials/mod-speakers.php
+++ partials/mod-speakers.php
@@ -1,23 +1,21 @@
 <?php $module = get_module_by_slug('speakers') ?>
-<?php $speakers = new WP_Query( array('post_type' => 'speaker', 'orderby' => 'menu_order', 'posts_per_page' => -1) ); ?>
+<?php $speakers = new WP_Query( array('post_type' => 'speaker', 'orderby' => 'menu_order', 'posts_per_page' => -1, 'cat' => 4) ); ?>
 
 <div id="module-speakers">
 	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<?php if ( $speakers->have_posts() ) : ?>
-					<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
-						<?php if (has_post_thumbnail()): ?>
-							<div class="tile">
-								<?php the_post_thumbnail( '200x200', $attr ); ?>
-								<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-								<h6><?php the_field('association') ?></h6>
-							</div>
-						<?php endif;?>
-					<?php endwhile; ?>
-					<?php wp_reset_postdata(); ?>
-				<?php endif; ?>
-			</div>
+		<div class="row row-eq-height">
+			<?php if ( $speakers->have_posts() ) : ?>
+				<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
+					<div class="col-xs-12 col-sm-6 col-md-2">
+						<div class="tile">
+							<?php the_post_thumbnail( '200x200', $attr ); ?>
+							<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+							<h6><?php the_field('association') ?></h6>
+						</div>
+					</div>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>

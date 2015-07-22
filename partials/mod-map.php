5c0cb0c3c5fb579affac9ed3477f322a9a2e28f2
Cody Marx Bailey <superphly@gmail.com>
2015-07-21 20:25:25 -0500

    no message

--- partials/mod-map.php
+++ partials/mod-map.php
@@ -26,7 +26,9 @@
 				<?php if ($past_bils->have_posts() ) : ?>
 					<ul>
 						<?php while ( $past_bils->have_posts() ) : $past_bils->the_post(); ?>
-							<li><?php the_title() ?> - <?php the_field('general_location', $post->ID); ?></li>
+							<li>
+								<a href="<?php the_permalink(); ?>"><strong><?php the_title() ?></strong> - <?php the_field('general_location', $post->ID); ?></a>
+							</li>
 						<?php endwhile; ?>
 					</ul>
 					<?php wp_reset_postdata(); ?>

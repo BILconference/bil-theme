2f7273b7d1b33229ed53c935f3c78c8a3e6062c3
Michael Cummings <michaeldcummings@gmail.com>
2015-07-24 02:35:02 -0500

    menus

--- inc/menus.php
+++ inc/menus.php
@@ -7,6 +7,10 @@
 	register_nav_menus(
 		array(
 			'main-nav' => __( 'The Main Menu', 'bonestheme' ),   // main nav in header
+			'footer-links' => __( 'Footer Links', 'bonestheme' ), // secondary nav in footer
+			'footer-links2'=> __( 'Footer Links 2', 'bonestheme' ),
+			'footer-links3'=> __( 'Footer Links 3', 'bonestheme' )
+
 		)
 	);
 

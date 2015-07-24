66b3bec4cbd54a6e9645c3bb653ecb710134b9cd
Michael Cummings <michaeldcummings@gmail.com>
2015-07-24 02:56:19 -0500

    -

--- footer.php
+++ footer.php
@@ -34,7 +34,16 @@
 							)); ?>
 						<p>Press, Contact, Resources, Media Kit, Sponsorship Stuff</p>
 					</div>
-					<div class="col-xs-12 col-sm-4">
+
+					<div class="col-xs-12 col-sm-4" style="padding: 2em">
+						<a href="https://en.wikipedia.org/wiki/BIL_Conference" class="image">
+							<img alt="Wp wordmark pos rgb.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/175px-Wp_wordmark_pos_rgb.svg.png" width="175" height="44" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/263px-Wp_wordmark_pos_rgb.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/350px-Wp_wordmark_pos_rgb.svg.png 2x" data-file-width="132" data-file-height="33">
+						</a> 
+						<a href="http://creativecommons.org/" rel="nofollow">
+							<img alt="CreativeCommons logo trademark.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/7/7a/CreativeCommons_logo_trademark.svg/150px-CreativeCommons_logo_trademark.svg.png" width="150" height="39" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/7/7a/CreativeCommons_logo_trademark.svg/225px-CreativeCommons_logo_trademark.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/7/7a/CreativeCommons_logo_trademark.svg/300px-CreativeCommons_logo_trademark.svg.png 2x" data-file-width="280" data-file-height="72">
+						</a>
+					</div>
+					<div class="col-xs-12">
 						<section class="meta">
 						    <p class="colophon"><img src="/wp-content/uploads/2015/07/cc.large_.png" class="tiny-cc-logo" alt="creative commons icon"/> 2007-<?php echo date('Y');?> BIL Conference </p>
 						 </section>

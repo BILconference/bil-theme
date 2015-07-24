9abfa050193dd2d5436ba3aef9a178b8bf4329d0
Cody Marx Bailey <superphly@gmail.com>
2015-07-24 16:57:01 -0500

    no message

--- partials/mod-intro.php
+++ partials/mod-intro.php
@@ -6,7 +6,7 @@
 <div id="module-intro" style="background-image: url('<?php echo $image; ?>')">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 borders">
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 content">
 				<?php echo $text ?>
 			</div>
 		</div>

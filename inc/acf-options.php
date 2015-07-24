d1554caa613cf182f3657bd3ba552ff2f65df660
Cody Marx Bailey <superphly@gmail.com>
2015-07-24 15:08:06 -0500

    no message

--- inc/acf-options.php
+++ inc/acf-options.php
@@ -3,8 +3,8 @@
 if( function_exists('acf_add_options_sub_page') ) {
 
     acf_add_options_sub_page(array(
-        'title' => 'Press Options',
-        'menu' => 'Options',
+        'title' => 'Options',
+        'menu' => 'Press Options',
         'parent' => 'edit.php?post_type=press',
         'slug' => 'press_options',
         'capability' => 'manage_options'

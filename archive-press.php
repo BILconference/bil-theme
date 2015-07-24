6909890a5b65d1c9ce98601ce6bbfd0f6281cbf4
Cody Marx Bailey <superphly@gmail.com>
2015-07-24 14:08:33 -0500

    no message

--- archive-press.php
+++ archive-press.php
@@ -1,16 +1,27 @@
 <?php get_header(); ?>
 
+	<div id="press-hero" class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<?php $image = the_field('press_image', 'option'); ?>
+					<?php if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div id="press-list">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
 					<h1>Press</h1>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12">
+					<ul>
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> by <a href="<?php the_field('url') ?>"><?php the_field('publisher'); ?></h3>
+						<li>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> by <a href="<?php the_field('url') ?>"><?php the_field('publisher'); ?></a>
+						</li>
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

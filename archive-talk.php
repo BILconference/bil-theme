962d73b0affd068757d7b6b85c0979a111df980d
Cody Marx Bailey <superphly@gmail.com>
2015-07-26 15:43:23 -0500

    no message

--- archive-talk.php
+++ archive-talk.php
@@ -10,12 +10,11 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-
-						<?php $speaker = get_field('speaker'); ?>
-						<?php if( $speaker ) { ?>
+						<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+						<?php if (get_field('speaker')) { ?>
+							<?php $speaker = get_field('speaker'); ?>
 							<a href="<?php echo get_permalink( $speaker->ID ); ?>">
-								<?php echo get_the_title( $speaker->ID ); ?>
+								<?php echo $speaker->post_title; ?>
 							</a>
 						<?php } ?>
 					<?php endwhile; endif; ?>

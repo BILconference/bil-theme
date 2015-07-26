aae8b3a95c373f6b7153f3ae5b4d6f6cc1664f7a
Cody Marx Bailey <superphly@gmail.com>
2015-07-26 15:50:29 -0500

    no message

--- archive-talk.php
+++ archive-talk.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
+	<div id="talk-hero" class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<?php $image = get_field('talk_image', 'option'); ?>
+					<?php if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div id="talk-list">
 		<div class="container">
 			<div class="row">
@@ -10,13 +23,21 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+						<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 						<?php if (get_field('speaker')) { ?>
 							<?php $speaker = get_field('speaker'); ?>
 							<a href="<?php echo get_permalink( $speaker->ID ); ?>">
 								<?php echo $speaker->post_title; ?>
 							</a>
 						<?php } ?>
+
+						<?php if (get_field('event')) { ?>
+							<?php $event = get_field('event'); ?>
+							<a href="<?php echo get_permalink( $event->ID ); ?>">
+								@ <?php echo $event->post_title; ?>
+							</a>
+						<?php } ?>
+
 					<?php endwhile; endif; ?>
 				</div>
 			</div>

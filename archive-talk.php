9b0c9f3b138b85fca05d98f0a301f34897096952
Cody Marx Bailey <superphly@gmail.com>
2015-07-26 15:53:16 -0500

    no message

--- archive-talk.php
+++ archive-talk.php
@@ -21,8 +21,8 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12">
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div class="col-xs-12 col-sm-6 col-md-4">
 						<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 						<?php if (get_field('speaker')) { ?>
 							<?php $speaker = get_field('speaker'); ?>
@@ -37,9 +37,8 @@
 								@ <?php echo $event->post_title; ?>
 							</a>
 						<?php } ?>
-
-					<?php endwhile; endif; ?>
-				</div>
+					</div>
+				<?php endwhile; endif; ?>
 			</div>
 		</div>
 	</div>

548179199b5f0f5406474cdb1cc7f76dde5cbcf8
Michael Cummings <michaeldcummings@gmail.com>
2015-07-26 13:55:06 -0500

    footer stuff

--- footer.php
+++ footer.php
@@ -18,6 +18,7 @@
 							)); ?>
 						</nav>
 					</div>
+
 					<div class="col-xs-12 col-sm-4">
 						<?php wp_nav_menu(array(
 								'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
@@ -35,7 +36,26 @@
 						<p>Press, Contact, Resources, Media Kit, Sponsorship Stuff</p>
 					</div>
 
-					<div class="col-xs-12 col-sm-4" style="padding: 2em">
+					<div class="col-xs-12 col-sm-4">
+						<?php wp_nav_menu(array(
+								'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+								'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+								'menu' => __( 'Footer Links 3', 'bonestheme' ),   // nav name
+								'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+								'theme_location' => 'footer-links3',             // where it's located in the theme
+								'before' => '',                                 // before the menu
+								'after' => '',                                  // after the menu
+								'link_before' => '',                            // before each link
+								'link_after' => '',                             // after each link
+								'depth' => 0,                                   // limit the depth of the nav
+								'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+							)); ?>
+						<p>Press, Contact, Resources, Media Kit, Sponsorship Stuff</p>
+					</div>
+
+				</div>
+
+					<div class="col-xs-12" style="padding: 2em">
 						<a href="https://en.wikipedia.org/wiki/BIL_Conference" class="image">
 							<img alt="Wp wordmark pos rgb.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/175px-Wp_wordmark_pos_rgb.svg.png" width="175" height="44" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/263px-Wp_wordmark_pos_rgb.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/0/03/Wp_wordmark_pos_rgb.svg/350px-Wp_wordmark_pos_rgb.svg.png 2x" data-file-width="132" data-file-height="33">
 						</a> 

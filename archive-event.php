c9520392069ee3cca5c17fa2e3152b6e65869fd4
Michael Cummings <michaeldcummings@gmail.com>
2015-07-25 21:09:33 -0500

    -

--- archive-event.php
+++ archive-event.php
@@ -8,7 +8,7 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-xs-12 col-sm-8">
 					<?php $upcoming_bils = upcoming_bils(); ?>
 					<?php if ($upcoming_bils->have_posts() ) : ?>
 						<h1>Upcoming BILs</h1>
@@ -31,13 +31,22 @@
 							<?php while ( $past_bils->have_posts() ) : $past_bils->the_post(); ?>
 								<li>
 									<a href="<?php the_permalink(); ?>"><strong><?php the_title() ?></strong> - <?php the_field('general_location'); ?></a>
-									<?php the_field('flag'); ?>
+									<?php $flag = the_field('flag'); ?>
+									<img src="<?php echo $flag[6]; ?>"/>
 								</li>
 							<?php endwhile; ?>
 						</ul>
 						<?php wp_reset_postdata(); ?>
 					<?php endif; ?>
 
+				</div>
+
+				<div class="sidebar col-xs-12 col-sm-4">
+					<?php //this is a place we could put this content in as a widget ?>
+					<div class="sidebar-cta host-a-bil">
+						Don't see your country or city represented?
+						<button class="btn button btn-large">Host your own</button>
+					</div>
 				</div>
 			</div>
 		</div>

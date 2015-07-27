2a596c6fef6c31e7aedd4da491a7d17ebc8b4564
Michael Cummings <michaeldcummings@gmail.com>
2015-07-27 00:45:43 -0500

    -

--- inc/helpers.php
+++ inc/helpers.php
@@ -103,7 +103,7 @@ function get_youtube_video_id($youtube) {
 	// pare string for query variables and assign to array
 	parse_str( $parts['query'], $params );
 	var_dump($params);
-	var_dump($params[0]);
+	var_dump($params['v']);
 
 	if ( in_array( 'v', $params ) ){
 		return end( explode( '=', $params['v'] ) );

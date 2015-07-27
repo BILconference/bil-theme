0d000116f4bdb52eee10d761219d6559e7cc6308
Michael Cummings <michaeldcummings@gmail.com>
2015-07-27 14:40:34 -0500

    -

---
+++ single.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-8">
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</div>
+				<div class="col-xs-12 col-md-4">
+					sidebar
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>

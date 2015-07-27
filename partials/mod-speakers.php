c238feeac630848d64dc7e96f36c15e7ecc3fa53
Cody Marx Bailey <superphly@gmail.com>
2015-07-27 14:07:13 -0500

    no message

--- partials/mod-speakers.php
+++ partials/mod-speakers.php
@@ -25,7 +25,14 @@
 				<?php while ( $speakers->have_posts() ) : $speakers->the_post(); ?>
 					<div class="speaker col-xs-12 col-sm-3 col-md-2">
 						<a href="<?php the_permalink();?>">
-							<?php the_post_thumbnail( '200x200' ); ?>
+							<?php if ( has_post_thumbnail() ) {
+								$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '300x300' ); 
+								if ($image) {
+									echo '<img src="' . $image[0] . '" class="img-responsive" />';
+								}
+							} else {
+								echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/library/images/blank-person.png" class="img-responsive" />';
+							} ?>
 							<h4><?php the_title(); ?></h4>
 							<h5><?php the_field('association') ?></h5>
 						</a>

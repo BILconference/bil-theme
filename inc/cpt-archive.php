547cae271f968b9856ac76a2cacdcf23f013ed58
Cody Marx Bailey <superphly@gmail.com>
2015-07-27 02:57:58 -0500

    no message

--- inc/cpt-archive.php
+++ inc/cpt-archive.php
@@ -15,7 +15,7 @@
 // Speaker Archive
 
 function cpt_archive_speaker($query) {
-    if ($query->is_main_query() && $query->is_post_type_archive('speaker') && !is_admin())
+    if ($query->is_main_query() && $query->is_post_type_archive('speaker') && !is_admin()) {
         $taxquery = array(
 			array(
 				'taxonomy' => 'group',
@@ -26,6 +26,7 @@ function cpt_archive_speaker($query) {
 		$query->set('tax_query', $taxquery );
 		$query->set('orderby', 'menu_order');
 		$query->set('posts_per_page', -1);
+	}
 }
  
 add_action('pre_get_posts', 'cpt_archive_speaker');
@@ -35,8 +36,9 @@ add_action('pre_get_posts', 'cpt_archive_speaker');
 // Event Archive
 
 function cpt_archive_event($query) {
-    if ($query->is_main_query() && $query->is_post_type_archive('event') && !is_admin())
+    if ($query->is_main_query() && $query->is_post_type_archive('event') && !is_admin()) {
         $query->set('posts_per_page', -1);
+    }
 }
  
 add_action('pre_get_posts', 'cpt_archive_event');
@@ -46,8 +48,9 @@ add_action('pre_get_posts', 'cpt_archive_event');
 // Talk Archive
 
 function cpt_archive_talk($query) {
-    if ($query->is_main_query() && $query->is_post_type_archive('talk') && !is_admin())
+    if ($query->is_main_query() && $query->is_post_type_archive('talk') && !is_admin()) {
         $query->set('posts_per_page', -1);
+    }
 }
  
 add_action('pre_get_posts', 'cpt_archive_talk');

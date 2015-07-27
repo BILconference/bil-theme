0467b41ea538b84a7853de746ff268ee8a046df6
Michael Cummings <michaeldcummings@gmail.com>
2015-07-27 14:47:55 -0500

    adding featured image to new posts

--- single.php
+++ single.php
@@ -1,10 +1,20 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div class="content">
+	<div id="" class="single-post content">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-md-8">
+				<?php if ( has_post_thumbnail() ) { ?>
+				<?php $attr = array(
+								'class' => 'img-responsive'
+					  ); 
+				?>
+					<div class='post-featured-image'>
+						<?php the_post_thumbnail( 'large', $attr );  ?>
+					</div>
+				<?php } ?>
+
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				</div>

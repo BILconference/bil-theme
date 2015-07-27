d969ae90f1b2ab360b5fa86a3c7921e095c0e5fb
Michael Cummings <michaeldcummings@gmail.com>
2015-07-27 01:40:05 -0500

    -

--- single-talk.php
+++ single-talk.php
@@ -19,27 +19,30 @@
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
 
-			<div class="row">
-				<div class="col-xs-12">
-					<h1><?php the_title(); ?></h1>
+	<div id="talk-details" class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1><?php the_title(); ?></h1>
 
-					<p>
-						<?php if( $speaker ) { ?>
-							<a href="<?php echo get_permalink( $speaker->ID ); ?>"><?php echo get_the_title( $speaker->ID ); ?></a>
-						<?php } else { ?> unknown <?php } ?>
-						&nbsp;at&nbsp;
-						<?php if( $event ) { ?>
-							<a href="<?php echo get_permalink( $event->ID ); ?>">
-								<?php echo get_the_title( $event->ID ); ?>
-							</a>
-						<?php } else { ?> unknown <?php } ?>
-					</p>
+				<p>
+					<?php if( $speaker ) { ?>
+						<a href="<?php echo get_permalink( $speaker->ID ); ?>"><?php echo get_the_title( $speaker->ID ); ?></a>
+					<?php } else { ?> unknown <?php } ?>
+					&nbsp;at&nbsp;
+					<?php if( $event ) { ?>
+						<a href="<?php echo get_permalink( $event->ID ); ?>">
+							<?php echo get_the_title( $event->ID ); ?>
+						</a>
+					<?php } else { ?> unknown <?php } ?>
+				</p>
 
-					<?php the_content(); ?>
-				</div>
+				<?php the_content(); ?>
 			</div>
 		</div>
+		
 	</div>
 
 <?php endwhile; endif; ?>

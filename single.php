ead9a3af4298208228bc04286dc8a548fc67c840
Michael Cummings <michaeldcummings@gmail.com>
2015-07-27 15:00:21 -0500

    -

--- single.php
+++ single.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php 
+	$author_email = get_the_author_meta( 'user_email' ); 
+	$author_name = get_the_author_meta( 'display_name' );
+	$author_image = get_avatar( get_the_author_meta( 'ID' ), 64 );
+	$author_bio = get_the_author_meta( 'description' );
+?>
+
+
+
 	<div id="" class="single-post content">
 		<div class="container">
 			<div class="row">
@@ -18,6 +27,19 @@
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				</div>
+
+				<div class="author">
+					<div class="row">
+						<div class="col-xs-12 col-sm-4">
+							<?php echo $author_image; ?>
+						</div>
+						<div class="col-xs-12 col-sm-8">
+							<span class="author-name"><?php echo $author_name; ?></span>
+							<p class="author-bio"><?php echo $author_bio; ?></p>
+						</div>
+					</div>
+				</div>
+
 				<div class="col-xs-12 col-md-4">
 					sidebar
 				</div>

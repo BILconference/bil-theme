0c58f6566c1aa5d82f484af5f7a65048fb5a4499
Cody Marx Bailey <superphly@gmail.com>
2015-07-27 21:49:33 -0500

    no message

--- inc/init.php
+++ inc/init.php
@@ -1,15 +1,34 @@
 <?php
 
-function themeInit() {
+function bones_head_cleanup() {
+	remove_action( 'wp_head', 'feed_links_extra');			// Display the links to the extra feeds such as category feeds
+	remove_action( 'wp_head', 'feed_links'); 				// Display the links to the general feeds: Post and Comment Feed
+	remove_action( 'wp_head', 'rsd_link'); 					// Display the link to the Really Simple Discovery service endpoint, EditURI link
+	remove_action( 'wp_head', 'wlwmanifest_link');			// Display the link to the Windows Live Writer manifest file.
+	remove_action( 'wp_head', 'index_rel_link');			// index link
+	remove_action( 'wp_head', 'parent_post_rel_link');		// prev link
+	remove_action( 'wp_head', 'start_post_rel_link');		// start link
+	remove_action( 'wp_head', 'adjacent_posts_rel_link'); 	// Display relational links for the posts adjacent to the current post.
+	remove_action( 'wp_head', 'wp_generator'); 				// Display the XHTML generator that is generated on the wp_head hook, WP version
 
+	// remove WP version from css
+	add_filter( 'style_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
+	// remove Wp version from scripts
+	add_filter( 'script_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
+}
+
+add_action( 'init', 'bones_head_cleanup' );
+
+
+
+// Theme Initialization
+
+function themeInit() {
 	// allow editor style
 	add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
 
 	// let's get language support going, if you need it
 	load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
-
-	// launching operation cleanup (library/bones.php)
-	add_action( 'init', 'bones_head_cleanup' );
 
 	// a better title (library/bones.php)
 	add_filter( 'wp_title', 'rw_title', 10, 3 );
@@ -40,7 +59,6 @@ function themeInit() {
 
 	// add filter to prevent "slack.png" from occupying the "slack" slug
 	add_filter( 'wp_unique_post_slug_is_bad_attachment_slug', '__return_true' );
-
 
 }
 
--- library/bones.php
+++ library/bones.php
@@ -1,22 +1,5 @@
 <?php
 
-function bones_head_cleanup() {
-	remove_action( 'wp_head', 'feed_links_extra');			// Display the links to the extra feeds such as category feeds
-	remove_action( 'wp_head', 'feed_links'); 				// Display the links to the general feeds: Post and Comment Feed
-	remove_action( 'wp_head', 'rsd_link'); 					// Display the link to the Really Simple Discovery service endpoint, EditURI link
-	remove_action( 'wp_head', 'wlwmanifest_link');			// Display the link to the Windows Live Writer manifest file.
-	remove_action( 'wp_head', 'index_rel_link');			// index link
-	remove_action( 'wp_head', 'parent_post_rel_link');		// prev link
-	remove_action( 'wp_head', 'start_post_rel_link');		// start link
-	remove_action( 'wp_head', 'adjacent_posts_rel_link'); 	// Display relational links for the posts adjacent to the current post.
-	remove_action( 'wp_head', 'wp_generator'); 				// Display the XHTML generator that is generated on the wp_head hook, WP version
-
-	// remove WP version from css
-	add_filter( 'style_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
-	// remove Wp version from scripts
-	add_filter( 'script_loader_src', 'bones_remove_wp_ver_css_js', 9999 );
-}
-
 // A better title
 // http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html
 function rw_title( $title, $sep, $seplocation ) {

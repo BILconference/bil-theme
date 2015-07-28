dbd1394259806c0adedd2b2c0fbc144f9897a6e8
Cody Marx Bailey <superphly@gmail.com>
2015-07-28 11:39:01 -0500

    no message

--- inc/fonts.php
+++ inc/fonts.php
@@ -3,7 +3,6 @@
 function bil_fonts() {
 	wp_enqueue_style('hind', 'http://fonts.googleapis.com/css?family=Hind:700,400,300');
 	wp_enqueue_style('opensans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,700,300');
-
 }
 
 add_action('wp_enqueue_scripts', 'bil_fonts');

1d76d8f4e2f84162a30557997c90ad91391b0849
Cody Marx Bailey <superphly@gmail.com>
2015-07-29 21:38:57 -0500

    no message

--- page.php
+++ page.php
@@ -4,11 +4,11 @@
 	<div class="content">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-md-8">
+				<div class="col-xs-12 col-md-8 col-md-push-4">
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				</div>
-				<div class="col-xs-12 col-md-4">
+				<div class="col-xs-12 col-md-4 col-md-pull-8">
 					sidebar
 				</div>
 			</div>

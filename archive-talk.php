5cd2f3d028e84667c6c873b237098208c6c3cc75
Cody Marx Bailey <superphly@gmail.com>
2015-07-29 23:28:16 -0500

    no message

--- archive-talk.php
+++ archive-talk.php
@@ -1,18 +1,5 @@
 <?php get_header(); ?>
 
-<?php $image = get_field('talk_image', 'option'); ?>
-<?php if( !empty($image) ): ?>
-	<div id="talks-hero" class="jumbotron">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-				</div>
-			</div>
-		</div>
-	</div>
-<?php endif; ?>
-
 <?php get_template_part('partials/list-talks'); ?>
 
 <?php get_footer(); ?>

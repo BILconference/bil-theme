75a5b573cb8017422618c441ef4087fd5bb6a848
Cody Marx Bailey <superphly@gmail.com>
2015-07-29 22:19:13 -0500

    no message

--- page.php
+++ page.php
@@ -3,11 +3,11 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-md-9 col-md-push-3">
+				<div class="col-xs-12 col-md-9 col-md-push-3 content">
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				</div>
-				<div class="col-xs-12 col-md-3 col-md-pull-9">
+				<div class="col-xs-12 col-md-3 col-md-pull-9 sidebar">
 					<?php get_sidebar(); ?>
 				</div>
 			</div>

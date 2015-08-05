5c35c3d30441ae64bb17291926754d8da0249415
Cody Marx Bailey <superphly@gmail.com>
2015-08-05 08:00:18 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -1,42 +1,50 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div id="" class="single-post content">
+	<?php if (get_field('speakers_hero_background', 'option')) { 
+		$hero_bg_obj = get_field('speakers_hero_background', 'option');
+		$hero_bg = $hero_bg_obj['sizes']['full-width'];
+	} ?>
+
+	<div id="speakers-hero" class="hero jumbotron" style="background-image: url('<?php echo $hero_bg; ?>');" >
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-md-8">
-				<?php if ( has_post_thumbnail() ) { ?>
-				<?php $attr = array(
-								'class' => 'img-responsive'
-					  ); 
-				?>
-					<div class='post-featured-image'>
-						<?php the_post_thumbnail( 'full', $attr );  ?>
-					</div>
-				<?php } ?>
-
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				</div>
-
-				<div class="author col-xs-12 col-md-8">
-					<div class="row">
-						<div class="col-xs-12 col-sm-4">
-							
-						</div>
-						<div class="col-xs-12 col-sm-8">
-							<span class="author-name"><?php echo $author_name; ?></span>
-							<p class="author-bio"><?php echo $author_bio; ?></p>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-xs-12 col-md-4">
-					sidebar
+				<div class="col-xs-12">
+					<?php $image = get_field('speakers_hero_foreground', 'option'); ?>
+					<?php if( !empty($image) ): ?>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
 	</div>
-<?php endwhile; endif; ?>
+	<div id="speakers-list">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<h1>Speakers</h1>
+					<?php the_field('speakers_content', 'option'); ?>
+				</div>
+				<div class="col-xs-12 col-sm-8">
+					<div class="row">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<div class="col-xs-12 col-sm-6 col-md-4 speaker">
+								<a href="<?php the_permalink(); ?>">
+									<?php if ( has_post_thumbnail() ) {
+										$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '300x300' ); 
+										if ($image) {
+											echo '<img src="' . $image[0] . '" class="img-responsive" />';
+										}
+									} else {
+										echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/library/images/blank-person.png" class="img-responsive" />';
+									} ?>
+									<h3><?php the_title(); ?></h3>
+								</a>
+							</div>
+						<?php endwhile; endif; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
 
 <?php get_footer(); ?>

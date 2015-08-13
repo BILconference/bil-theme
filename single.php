9430f73e702730ec76b5ca648d5d174238037f03
Cody Marx Bailey <superphly@gmail.com>
2015-08-13 16:47:12 -0500

    no message

--- single.php
+++ single.php
@@ -6,16 +6,16 @@
 				<div class="col-xs-12 col-md-9 col-md-push-3 content">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<?php if ( has_post_thumbnail() ) { ?>
-						<?php $attr = array(
-										'class' => 'img-responsive'
-							  ); 
-						?>
+						<?php $attr = array('class' => 'img-responsive');?>
 							<div class='post-featured-image'>
 								<?php the_post_thumbnail( 'full', $attr );  ?>
 							</div>
 						<?php } ?>
 
 						<h1><?php the_title(); ?></h1>
+						<p class="by-line">
+							<?php the_time('j F Y') ?> by <?php the_author() ?>
+						</p>
 						<?php the_content(); ?>
 					<?php endwhile; endif; ?>
 				</div>	

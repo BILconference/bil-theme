2fd4d77dbcaf06c737d558cce7b322bf6be8f760
Cody Marx Bailey <superphly@gmail.com>
2015-08-13 16:18:55 -0500

    no message

---
+++ archive.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+	<div id="blog-list">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-9 col-md-push-3 content">
+					<div class="row">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<div class="col-xs-12 col-sm-6 col-md-4 speaker">
+								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								<?php the_excerpt(); ?>
+							</div>
+						<?php endwhile; endif; ?>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-3 col-md-pull-9 sidebar">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+	</div>	
+
+<?php get_footer(); ?>

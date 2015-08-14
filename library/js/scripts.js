f6dc8e31b28e1b4cd5a9e6551cae771f330556d7
Cody Marx Bailey <superphly@gmail.com>
2015-08-14 17:41:42 -0500

    no message

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -45,72 +45,22 @@ var waitForFinalEvent = (function () {
 var timeToWaitForLast = 100;
 
 
-/*
- * Here's an example so you can see how we're using the above function
- *
- * This is commented out so it won't work, but you can copy it and
- * remove the comments.
- *
- *
- *
- * If we want to only do it on a certain page, we can setup checks so we do it
- * as efficient as possible.
- *
- * if( typeof is_home === "undefined" ) var is_home = $('body').hasClass('home');
- *
- * This once checks to see if you're on the home page based on the body class
- * We can then use that check to perform actions on the home page only
- *
- * When the window is resized, we perform this function
- * $(window).resize(function () {
- *
- *    // if we're on the home page, we wait the set amount (in function above) then fire the function
- *    if( is_home ) { waitForFinalEvent( function() {
- *
- *	// update the viewport, in case the window size has changed
- *	viewport = updateViewportDimensions();
- *
- *      // if we're above or equal to 768 fire this off
- *      if( viewport.width >= 768 ) {
- *        console.log('On home page and window sized to 768 width or more.');
- *      } else {
- *        // otherwise, let's do this instead
- *        console.log('Not on home page, or window sized to less than 768.');
- *      }
- *
- *    }, timeToWaitForLast, "your-function-identifier-string"); }
- * });
- *
- * Pretty cool huh? You can create functions like this to conditionally load
- * content and other stuff dependent on the viewport.
- * Remember that mobile devices and javascript aren't the best of friends.
- * Keep it light and always make sure the larger viewports are doing the heavy lifting.
- *
-*/
-
-/*
- * We're going to swap out the gravatars.
- * In the functions.php file, you can see we're not loading the gravatar
- * images on mobile to save bandwidth. Once we hit an acceptable viewport
- * then we can swap out those images since they are located in a data attribute.
-*/
-function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
+// function loadGravatars() {
+//   // set the viewport using the function above
+//   viewport = updateViewportDimensions();
+//   // if the viewport is tablet or larger, we load in the gravatars
+//   if (viewport.width >= 768) {
+//   jQuery('.comment img[data-gravatar]').each(function(){
+//     jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
+//   });
+// 	}
+// }
 
 
 /*
  * Put all your regular jQuery in here.
 */
 jQuery(document).ready(function($) {
-
 
 	// Allows us to use a class to create DIV TEXT FILLING GOODNESS. 
 	$(".bigtext").bigtext();
@@ -132,27 +82,18 @@ jQuery(document).ready(function($) {
 	  $(this).addClass('active');
 	});
 
-	// Scroll Spy for Resources Menu and About Menu
-	$('body').scrollspy({ target: '.sidebar', offset:80 });
+ 	//  var clicked = false;
+	// $('.sidebar li a').click(
+	// function(){
+	// 	//$('#mycontent > div > h2').css('padding-top',0);
 
-  	var clicked = false;
-	$('.sidebar li a').click(
-	function(){
-		//$('#mycontent > div > h2').css('padding-top',0);
+	// 	// I want to kill this next line. It adds that padding, but can be done a better way so when you scroll to the top it doesn't look like shit.
+	// 	// https://www.dropbox.com/s/6ilpfzro6xej5b4/Screenshot%202015-08-14%2016.41.40.png?dl=0
 
-		// I want to kill this next line. It adds that padding, but can be done a better way so when you scroll to the top it doesn't look like shit.
-		// https://www.dropbox.com/s/6ilpfzro6xej5b4/Screenshot%202015-08-14%2016.41.40.png?dl=0
-
-		$( $( this ).attr('href') ).css('padding-top','50px');
-		clicked = true;
-		}
-	);	
-
-	$('body').on('activate.bs.scrollspy', function () {
-	  console.log('scrolling...');
-	  //if(!clicked)$('#mycontent > div > h2').css('padding-top',0);
-	  clicked = false;
-	});
+	// 	$( $( this ).attr('href') ).css('padding-top','50px');
+	// 	clicked = true;
+	// 	}
+	// );	
 
 	$('#menu_affix').affix({
 		offset: {

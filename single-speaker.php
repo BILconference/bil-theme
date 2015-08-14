b3d93630d5c7f35bfd472cfe64fdd582579222cb
Cody Marx Bailey <superphly@gmail.com>
2015-08-14 15:00:05 -0500

    no message

--- single-speaker.php
+++ single-speaker.php
@@ -14,41 +14,37 @@
 			</div>
 			<div class="col-xs-12 col-md-9 content">
 				<div id="speaker-about">
-					<div class="row">
-						<div class="col-xs-12 col-md-6">
-							<h2>Biography</h2>
-							<?php the_content(); ?>
-						</div>
-						<div class="col-xs-12 col-md-6">
-							<h2>Talks Given</h2>
-							<?php $talks = get_posts(array(
-								'post_type' => 'talk',
-								'meta_query' => array(
-									array(
-										'key' => 'speaker', // name of custom field
-										'value' => $post->ID, // matches exaclty "123", not just 123. This prevents a match for "1234"
-										'compare' => '='
-									)
-								)
-							)); ?>
-							<?php if( $talks ): ?>
-								<ul>
-									<?php foreach( $talks as $talk ): ?>
-										<?php $event = get_field('event', $talk->ID); ?>
-										<li>
-											<a href="<?php echo get_permalink($talk->ID); ?>">
-												<?php echo get_the_title($talk->ID); ?>
-											</a>
-											 at 
-											<a href="<?php echo get_permalink($event->ID); ?>">
-												<?php echo $event->post_title; ?>
-											</a>
-										</li>
-									<?php endforeach; ?>
-								</ul>
-							<?php endif; ?>
-						</div>
-					</div>
+					<h2>Biography</h2>
+					<?php the_content(); ?>
+				</div>
+				<div id="talks-given">
+					<h2>Talks Given</h2>
+					<?php $talks = get_posts(array(
+						'post_type' => 'talk',
+						'meta_query' => array(
+							array(
+								'key' => 'speaker', // name of custom field
+								'value' => $post->ID, // matches exaclty "123", not just 123. This prevents a match for "1234"
+								'compare' => '='
+							)
+						)
+					)); ?>
+					<?php if( $talks ): ?>
+						<ul>
+							<?php foreach( $talks as $talk ): ?>
+								<?php $event = get_field('event', $talk->ID); ?>
+								<li>
+									<a href="<?php echo get_permalink($talk->ID); ?>">
+										<?php echo get_the_title($talk->ID); ?>
+									</a>
+									 at 
+									<a href="<?php echo get_permalink($event->ID); ?>">
+										<?php echo $event->post_title; ?>
+									</a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>

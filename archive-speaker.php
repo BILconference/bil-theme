e7f87f36f1f3b3ec37feee342cb3c6c1a9941b40
Cody Marx Bailey <superphly@gmail.com>
2015-08-14 14:01:02 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -25,7 +25,7 @@
 					<?php the_field('speakers_content', 'option'); ?>
 				</div>
 				<div class="col-xs-12 col-sm-8">
-					<div class="row">
+					<div class="row" id="#featured">
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 							<div class="col-xs-12 col-sm-6 col-md-4 speaker">
 								<a href="<?php the_permalink(); ?>">
@@ -42,11 +42,11 @@
 							</div>
 						<?php endwhile; endif; ?>
 					</div>
-					<div class="row">
+					<div class="row" id="non-featured">
 						<div class="col-xs-12">
 							<?php $nfs = non_featured_speakers(); ?>
 							<?php if ( $nfs->have_posts() ) { ?>
-								<div class="nfs_list">
+								<div>
 									<?php while ( $nfs->have_posts() ) : $nfs->the_post(); ?>
 										<span class="speaker"><?php the_title(); ?></span>
 									<?php endwhile; ?>

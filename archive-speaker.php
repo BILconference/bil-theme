79fbd25171a92dbcf4974b807cbf86da5d09fbe4
Cody Marx Bailey <superphly@gmail.com>
2015-08-14 14:06:16 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -25,7 +25,7 @@
 					<?php the_field('speakers_content', 'option'); ?>
 				</div>
 				<div class="col-xs-12 col-sm-8">
-					<div class="row" id="#featured">
+					<div class="row" id="featured">
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 							<div class="col-xs-12 col-sm-6 col-md-4 speaker">
 								<a href="<?php the_permalink(); ?>">

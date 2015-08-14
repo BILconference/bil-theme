82649c167dd4e146cd711c2464eb16f921717594
Cody Marx Bailey <superphly@gmail.com>
2015-08-14 14:11:05 -0500

    no message

--- archive-speaker.php
+++ archive-speaker.php
@@ -26,29 +26,33 @@
 				</div>
 				<div class="col-xs-12 col-sm-8">
 					<div class="row" id="featured">
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-							<div class="col-xs-12 col-sm-6 col-md-4 speaker">
-								<a href="<?php the_permalink(); ?>">
-									<?php if ( has_post_thumbnail() ) {
-										$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '300x300' ); 
-										if ($image) {
-											echo '<img src="' . $image[0] . '" class="img-responsive" />';
-										}
-									} else {
-										echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/library/images/blank-person.png" class="img-responsive" />';
-									} ?>
-									<h3><?php the_title(); ?></h3>
-								</a>
-							</div>
-						<?php endwhile; endif; ?>
+						<?php if (have_posts()) { ?>
+							<h2>Featured Past Speakers</h2>
+							<?php while (have_posts()) : the_post(); ?>
+								<div class="col-xs-12 col-sm-6 col-md-4 speaker">
+									<a href="<?php the_permalink(); ?>">
+										<?php if ( has_post_thumbnail() ) {
+											$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '300x300' ); 
+											if ($image) {
+												echo '<img src="' . $image[0] . '" class="img-responsive" />';
+											}
+										} else {
+											echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/library/images/blank-person.png" class="img-responsive" />';
+										} ?>
+										<h3><?php the_title(); ?></h3>
+									</a>
+								</div>
+							<?php endwhile; ?>
+						<?php } ?>
 					</div>
 					<div class="row" id="non-featured">
 						<div class="col-xs-12">
 							<?php $nfs = non_featured_speakers(); ?>
 							<?php if ( $nfs->have_posts() ) { ?>
+								<h2>Full List of Past Speakers</h2>
 								<div>
 									<?php while ( $nfs->have_posts() ) : $nfs->the_post(); ?>
-										<span class="speaker"><?php the_title(); ?></span>
+										<a href="<?php the_permalink(); ?>" class="speaker"><?php the_title(); ?></a>
 									<?php endwhile; ?>
 								</div>
 							<?php } ?>

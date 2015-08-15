91864c1be78c7d641e899cf93643f8dc702444f1
Cody Marx Bailey <superphly@gmail.com>
2015-08-15 12:57:50 -0500

    no message

--- partials/mod-hero.php
+++ partials/mod-hero.php
@@ -7,7 +7,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<img class="bil-logo" src="wp-content/uploads/2015/07/bil-white-500.png"/>
+				<img class="bil-logo img-responsive" src="wp-content/uploads/2015/07/bil-white-500.png"/>
 				<?php echo $text ?>
 			</div>
 		</div>

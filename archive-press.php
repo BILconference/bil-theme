b3640cf5abf6743a0edf96c42ab0bc1548e4f3b1
Michael Cummings <michaeldcummings@gmail.com>
2015-12-15 03:29:46 -0600

    fix for issue #40

--- archive-press.php
+++ archive-press.php
@@ -23,7 +23,7 @@
 					<ul>
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<li>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> by <a href="<?php the_field('url') ?>"><?php the_field('publisher'); ?></a>
+							<a href="<?php the_field('url') ?>"><?php the_title(); ?></a> by <a href="<?php the_field('url') ?>"><?php the_field('publisher'); ?></a>
 						</li>
 					<?php endwhile; endif; ?>
 				</div>

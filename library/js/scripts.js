cd58c6ee7cbd778c2533624f0ed4cae525a4c587
Michael Cummings <michaeldcummings@gmail.com>
2016-02-02 02:46:02 -0600

    -

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -80,6 +80,41 @@ jQuery(document).ready(function($) {
 	    debugmode:  true   // when true, errors will be logged to console
 	});
 
+	// Organizers See More Function
+	var $el, $ps, $up, totalHeight;
+
+	$(".organizers-list .more").click(function() {
+	      
+	  totalHeight = 0
+
+	  $el = $(this);
+	  $p  = $el.parent();
+	  $up = $p.parent();
+	  $ps = $up.find("p:not('.read-more')");
+	  
+	  // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
+	  $ps.each(function() {
+	    totalHeight += $(this).outerHeight();
+	  });
+	        
+	  $up
+	    .css({
+	      // Set height to prevent instant jumpdown when max height is removed
+	      "height": $up.height(),
+	      "max-height": 9999
+	    })
+	    .animate({
+	      "height": totalHeight
+	    });
+	  
+	  // fade out read-more
+	  $p.fadeOut();
+	  
+	  // prevent jump-down
+	  return false;
+	    
+	});
+
 
 	// FILTERS
 	// ToDo: We will want to make an Ajax call in future to grab all related taxonomy terms to the particular filter.

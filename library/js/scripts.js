1728ab551f74c0e162956507b40c1bdc98b12367
Michael Cummings <michaeldcummings@gmail.com>
2016-02-02 12:24:20 -0600

    -

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -114,20 +114,20 @@ jQuery(document).ready(function($) {
 		offset: {
 			bottom: function () {
 			return (this.bottom = $('#footer').outerHeight(true) + 50)
+			}
 		}
-	}
+	})
 
 
-	// Javascript to enable link to tab on single event page tabs
+	// Javascript to enable link to tab
 	var url = document.location.toString();
 	if (url.match('#')) {
-	    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show');
+	    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
 	} 
 
 	// Change hash for page-reload
 	$('.nav-tabs a').on('shown.bs.tab', function (e) {
 	    window.location.hash = e.target.hash;
-	});
-})
+	})
 
 });

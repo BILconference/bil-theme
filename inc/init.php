3f611d45a28c9d0b9fd9e4fc072d05f79b8f92a8
Michael Cummings <michaeldcummings@gmail.com>
2016-02-02 12:02:11 -0600

    -

--- inc/init.php
+++ inc/init.php
@@ -84,3 +84,30 @@ function add_theme_caps() {
     $role->add_cap( 'unfiltered_html' ); 
 }
 add_action( 'admin_init', 'add_theme_caps');
+
+
+function bil_move_admin_bar() { ?>
+    <style type="text/css">
+        body {
+            margin-top: -28px;
+            padding-bottom: 28px;
+        }
+        body.admin-bar #wphead {
+            padding-top: 0;
+        }
+        body.admin-bar #footer {
+            padding-bottom: 28px;
+        }
+        #wpadminbar {
+            top: auto !important;
+            bottom: 0;
+        }
+        #wpadminbar .quicklinks .menupop ul {
+            bottom: 28px;
+        }
+    </style>
+<?php }
+// on backend area
+//add_action( 'admin_head', 'bil_move_admin_bar' );
+// on frontend area
+add_action( 'wp_head', 'bil_move_admin_bar' );

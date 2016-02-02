c5f678003b6274b2775bcb60aa2e32e063a19f13
Michael Cummings <michaeldcummings@gmail.com>
2016-02-02 12:21:55 -0600

    -

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -116,6 +116,18 @@ jQuery(document).ready(function($) {
 			return (this.bottom = $('#footer').outerHeight(true) + 50)
 		}
 	}
+
+
+	// Javascript to enable link to tab on single event page tabs
+	var url = document.location.toString();
+	if (url.match('#')) {
+	    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show');
+	} 
+
+	// Change hash for page-reload
+	$('.nav-tabs a').on('shown.bs.tab', function (e) {
+	    window.location.hash = e.target.hash;
+	});
 })
 
 });

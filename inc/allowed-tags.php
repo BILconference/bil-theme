e06206f261e663539ecd31e0f87daa28c66ca81e
Cody Marx Bailey <superphly@gmail.com>
2016-02-14 14:21:38 -0800

    oops

---
+++ inc/allowed-tags.php
@@ -0,0 +1,27 @@
+<?php 
+
+add_filter( 'wp_kses_allowed_html', 'bil_allowed_tags', 1, 1);
+
+function esw_author_cap_filter( $allowedposttags ) {
+
+	if ( !current_user_can( 'publish_posts' ) ) {
+		return $allowedposttags;
+	} else {
+		$allowedposttags['iframe'] = array(
+			'align' => true,
+			'width' => true,
+			'height' => true,
+			'frameborder' => true,
+			'name' => true,
+			'src' => true,
+			'id' => true,
+			'class' => true,
+			'style' => true,
+			'scrolling' => true,
+			'marginwidth' => true,
+			'marginheight' => true,
+		);
+		
+		return $allowedposttags;
+	}
+}

ee0b110c25ed7345664aa4326ffc0d0d71cbf377
Cody Marx Bailey <superphly@gmail.com>
2017-01-06 00:07:57 -0600

    no message

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,12 +1,5 @@
-/*global jQuery *//*! 
-* FitText.js 1.1
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license 
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/(function(e){e.fn.fitText=function(t,n){var r=t||1,i=e.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},n);return this.each(function(){var t=e(this),n=function(){t.css("font-size",Math.max(Math.min(t.width()/(r*10),parseFloat(i.maxFontSize)),parseFloat(i.minFontSize)))};n(),e(window).on("resize",n)})}})(jQuery);
+/*global jQuery * FitText.js 1.1 */
+(function(e){e.fn.fitText=function(t,n){var r=t||1,i=e.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},n);return this.each(function(){var t=e(this),n=function(){t.css("font-size",Math.max(Math.min(t.width()/(r*10),parseFloat(i.maxFontSize)),parseFloat(i.minFontSize)))};n(),e(window).on("resize",n)})}})(jQuery);
 
 /*! jQuery slabtext plugin v2.3.1 MIT/GPL2 @freqdec */
 (function($){$.fn.slabText=function(options){var settings={fontRatio:0.78,forceNewCharCount:true,wrapAmpersand:true,headerBreakpoint:null,viewportBreakpoint:null,noResizeEvent:false,resizeThrottleTime:300,maxFontSize:999,postTweak:true,precision:3,minCharsPerLine:0};$("body").addClass("slabtexted");return this.each(function(){if(options){$.extend(settings,options)}var $this=$(this),keepSpans=$("span.slabtext",$this).length,words=keepSpans?[]:String($.trim($this.text())).replace(/\s{2,}/g," ").split(" "),origFontSize=null,idealCharPerLine=null,fontRatio=settings.fontRatio,forceNewCharCount=settings.forceNewCharCount,headerBreakpoint=settings.headerBreakpoint,viewportBreakpoint=settings.viewportBreakpoint,postTweak=settings.postTweak,precision=settings.precision,resizeThrottleTime=settings.resizeThrottleTime,minCharsPerLine=settings.minCharsPerLine,resizeThrottle=null,viewportWidth=$(window).width(),headLink=$this.find("a:first").attr("href")||$this.attr("href"),linkTitle=headLink?$this.find("a:first").attr("title"):"";if(!keepSpans&&minCharsPerLine&&words.join(" ").length<minCharsPerLine){return}var grabPixelFontSize=function(){var dummy=jQuery('<div style="display:none;font-size:1em;margin:0;padding:0;height:auto;line-height:1;border:0;">&nbsp;</div>').appendTo($this),emH=dummy.height();dummy.remove();return emH};var resizeSlabs=function resizeSlabs(){var parentWidth=$this.width(),fs;if(parentWidth===0){return}$this.removeClass("slabtextdone slabtextinactive");if(viewportBreakpoint&&viewportBreakpoint>viewportWidth||headerBreakpoint&&headerBreakpoint>parentWidth){$this.addClass("slabtextinactive");return}fs=grabPixelFontSize();if(!keepSpans&&(forceNewCharCount||fs!=origFontSize)){origFontSize=fs;var newCharPerLine=Math.min(60,Math.floor(parentWidth/(origFontSize*fontRatio))),wordIndex=0,lineText=[],counter=0,preText="",postText="",finalText="",slice,preDiff,postDiff;if(newCharPerLine!=0&&newCharPerLine!=idealCharPerLine){idealCharPerLine=newCharPerLine;while(wordIndex<words.length){postText="";while(postText.length<idealCharPerLine){preText=postText;postText+=words[wordIndex]+" ";if(++wordIndex>=words.length){break}}if(minCharsPerLine){slice=words.slice(wordIndex).join(" ");if(slice.length<minCharsPerLine){postText+=slice;preText=postText;wordIndex=words.length+2}}preDiff=idealCharPerLine-preText.length;postDiff=postText.length-idealCharPerLine;if((preDiff<postDiff)&&(preText.length>=(minCharsPerLine||2))){finalText=preText;wordIndex--}else{finalText=postText}finalText=$("<div/>").text(finalText).html();if(settings.wrapAmpersand){finalText=finalText.replace(/&amp;/g,'<span class="amp">&amp;</span>')}finalText=$.trim(finalText);lineText.push('<span class="slabtext">'+finalText+"</span>")}$this.html(lineText.join(" "));if(headLink){$this.wrapInner('<a href="'+headLink+'" '+(linkTitle?'title="'+linkTitle+'" ':"")+"/>")}}}else{origFontSize=fs}$("span.slabtext",$this).each(function(){var $span=$(this),innerText=$span.text(),wordSpacing=innerText.split(" ").length>1,diff,ratio,fontSize;if(postTweak){$span.css({"word-spacing":0,"letter-spacing":0})}ratio=parentWidth/$span.width();fontSize=parseFloat(this.style.fontSize)||origFontSize;$span.css("font-size",Math.min((fontSize*ratio).toFixed(precision),settings.maxFontSize)+"px");diff=!!postTweak?parentWidth-$span.width():false;if(diff){$span.css((wordSpacing?"word":"letter")+"-spacing",(diff/(wordSpacing?innerText.split(" ").length-1:innerText.length)).toFixed(precision)+"px")}});$this.addClass("slabtextdone")};resizeSlabs();if(!settings.noResizeEvent){$(window).resize(function(){if($(window).width()==viewportWidth){return}viewportWidth=$(window).width();clearTimeout(resizeThrottle);resizeThrottle=setTimeout(resizeSlabs,resizeThrottleTime)})}})}})(jQuery);
@@ -52,18 +45,6 @@ var waitForFinalEvent = (function () {
 var timeToWaitForLast = 100;
 
 
-// function loadGravatars() {
-//   // set the viewport using the function above
-//   viewport = updateViewportDimensions();
-//   // if the viewport is tablet or larger, we load in the gravatars
-//   if (viewport.width >= 768) {
-//   jQuery('.comment img[data-gravatar]').each(function(){
-//     jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-//   });
-// 	}
-// }
-
-
 /*
  * Put all your regular jQuery in here.
 */
@@ -72,18 +53,6 @@ jQuery(document).ready(function($) {
 	// Allows us to use a class to create DIV TEXT FILLING GOODNESS. 
 	$('.bigtext').bigtext();
 
-	// $('.single-event-nav-container').stickThis({
-	//     top:        0,      // top position of sticky element, measured from 'ceiling'
-	//     minscreenwidth: 960,      // element will not be sticky when viewport width smaller than this
-	//     maxscreenwidth: 999999,     // element will not be sticky when viewport width larger than this 
-	//     zindex:     1,      // z-index value of sticky element
-	//     debugmode:  true   // when true, errors will be logged to console
-	// });
-
-
-	// FILTERS
-	// ToDo: We will want to make an Ajax call in future to grab all related taxonomy terms to the particular filter.
-	// TALKS
 	var $btns = $('.btn').click(function() {
 	  if (this.id == 'all') {
 	    $('#parent > div').fadeIn(450);
@@ -97,18 +66,6 @@ jQuery(document).ready(function($) {
 	  $(this).addClass('active');
 	});
 
-	//  var clicked = false;
-	// $('.sidebar li a').click(
-	// function(){
-	// 	//$('#mycontent > div > h2').css('padding-top',0);
-
-	// 	// I want to kill this next line. It adds that padding, but can be done a better way so when you scroll to the top it doesn't look like shit.
-	// 	// https://www.dropbox.com/s/6ilpfzro6xej5b4/Screenshot%202015-08-14%2016.41.40.png?dl=0
-
-	// 	$( $( this ).attr('href') ).css('padding-top','50px');
-	// 	clicked = true;
-	// 	}
-	// );
 
 	$('#menu_affix').affix({
 		offset: {
@@ -117,11 +74,6 @@ jQuery(document).ready(function($) {
 			}
 		}
 	});
-
-	// $('.nav-tabs li a').click(function(e) {
-	// 	e.preventDefault();
-	// 	$(this).tab('show');
-	// });
 
 	// Javascript to enable link to tab
 	var url = document.location.toString();

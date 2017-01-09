d1a059321002b778c3548d558e49f1a77944558b
Michael Cummings <michaeldcummings@gmail.com>
2017-01-09 13:37:17 -0600

    updated streams template

--- archive-stream.php
+++ archive-stream.php
@@ -1,16 +1,35 @@
 <?php get_header(); ?>
 
-<?php 
-	$page = get_posts(
-				    array(
-				        'name'      => 'global',
-				        'post_type' => 'page'
-				    ) );
-	if ( $page )
-	{
-	    echo $page[0]->post_content;
-	}
+<div id="stream-list">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<h2>Global</h2>
+				<?php 
+					$page = get_posts(
+								    array(
+								        'name'      => 'global',
+								        'post_type' => 'page'
+								    ) );
+					if ( $page )
+					{
+					    echo $page[0]->post_content;
+					}
 
-?>
+				?>
+			</div>
+			<div class="col-xs-12 col-sm-8">
+				<ul>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<li>
+						<a href="<?php the_field('url') ?>"><?php the_title(); ?></a> by <a href="<?php the_field('url') ?>"><?php the_field('publisher'); ?></a>
+					</li>
+				<?php endwhile; endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 
 <?php get_footer(); ?>

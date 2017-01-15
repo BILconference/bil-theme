4dddcd25e554a53e5303da3ea681815b2c6ebb85
Cody Marx Bailey <superphly@gmail.com>
2017-01-14 23:42:22 -0600

    no message

--- partials/event-talks-before-during.php
+++ partials/event-talks-before-during.php
@@ -7,29 +7,46 @@
 			'value' => $post->ID, // matches exaclty "123", not just 123. This prevents a match for "1234"
 			'compare' => '='
 		)
-	)
+	),
+	'meta_key' => 'start_time',
+	'orderby' => 'meta_value'
 )); ?>
-xxxx
+
 <?php if($talks) { ?>
 	<div id="event-talks">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12">
-					<h2>Talks</h2>
-					<ul>
-						<?php foreach ( $talks as $talk ): ?>
-							<?php $speaker = get_field('speaker', $talk->ID); ?>
-							<li>
-								<a href="<?php echo get_permalink( $talk->ID ); ?>">
-									<?php echo $talk->post_title; ?>
-								</a>
-								 by 
-								<a href="<?php echo get_permalink($speaker->ID); ?>">
-									<?php echo $speaker->post_title; ?>
-								</a>
-							</li>
-						<?php endforeach; ?>
-					</ul>
+					<h2>Schedule</h2>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th>Time</th>
+									<th>Speaker</th>
+									<th>Title</th>
+								</tr>
+							</thead>
+							<?php foreach ( $talks as $talk ): ?>
+								<?php $speaker = get_field('speaker', $talk->ID); ?>
+								<tr>
+									<td><?php echo $talk->start_time; ?></td>
+									<td><?php echo $talk->start_time; ?></td>
+									<td>
+										<a href="<?php echo get_permalink( $talk->ID ); ?>">
+											<?php echo $talk->post_title; ?>
+										</a>
+									</td>
+									<td>
+										<a href="<?php echo get_permalink($speaker->ID); ?>">
+											<?php echo $speaker->post_title; ?>
+										</a>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>

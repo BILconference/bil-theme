b51d95adce8d1e1b5158b8d96982f18aea2c2e91
Cody Marx Bailey <superphly@gmail.com>
2017-01-16 00:52:01 -0600

    no message

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
 							<li><a class="soc-twitter" href="http://twitter.com/bilconf" target="_blank"></a></li>
 							<li><a class="soc-facebook" href="http://facebook.com/bilconference" target="_blank"></a></li>
 							<li class="hidden"><a class="soc-github" href="http://github.com/bilconference" target="_blank"></a></li>
-							<li class="slack hidden"><a href="/slack/"><img src="http://bil-slack.herokuapp.com/badge.svg"></a></li>
+							<li class="slack"><a href="/slack/"><img src="http://bil-slack.herokuapp.com/badge.svg"></a></li>
 						</ul>
 					</div>
 				</div>

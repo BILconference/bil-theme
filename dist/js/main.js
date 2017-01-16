9f83e3e6608e5ffc06e887d080e5ceb7531ab08b
Cody Marx Bailey <superphly@gmail.com>
2017-01-16 11:30:18 -0600

    no message

--- dist/js/main.js
+++ dist/js/main.js
@@ -4632,7 +4632,7 @@ jQuery(document).ready(function($) {
 	$('[data-date]').each(function(i, x) {
 		var fmt = $(x).data('format');
 		var date = moment.unix($(x).data(date));
-		var text = date.tz("America/Los_Angeles").format(fmt);
+		var text = date.tz(localtz).format(fmt);
 
 		$(x).text(text);
 	});

bf6142796d098d0f0594b96c3dbfa4ee10323df0
Cody Marx Bailey <superphly@gmail.com>
2017-01-17 02:06:56 -0600

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const concat = require('gulp-concat');
 const clean = require('gulp-clean');
 const rename = require('gulp-rename');
 const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const image = require('gulp-image');
+
+
+
 
 const jsFiles = [
 	'library/js/modernizr.js',
@@ -14,11 +20,27 @@ const jsFiles = [
 	'library/js/custom.js'
 ];
 
+const cssFiles = [
+	'library/scss/style.scss',
+	'library/scss/login.scss',
+	'library/scss/ie.scss',
+	'library/scss/editor-style.scss',
+	'library/scss/admin.scss'
+];
+
+const imageFiles = [ 'library/images/*' ]
+
+
 gulp.task('default', () => {
-	gulp.watch(jsFiles, ['scripts']);
+	gulp.watch(jsFiles, ['js']);
+	gulp.watch('library/scss/**/*.scss', ['css']);
+	gulp.watch(imageFiles, ['images']);
 });
 
-gulp.task('scripts', ['cleanJs'], () =>
+
+
+
+gulp.task('js', ['cleanJs'], () =>
 	gulp.src(jsFiles)
 	.pipe(concat('main.js'))
 	.pipe(gulp.dest('dist/js'))
@@ -32,3 +54,40 @@ gulp.task('cleanJs', () =>
   gulp.src(['dist/js'], { read: false })
 	.pipe(clean())
 );
+
+
+
+gulp.task('css', ['cleanCss'], () =>
+	gulp.src(cssFiles)
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('dist/css'))
+	.pipe(rename({ suffix: '.min' }))
+	.pipe(cleanCss({ debug: true }, (details) => {
+		console.log(`${details.name}: ${details.stats.originalSize}`);
+		console.log(`${details.name}: ${details.stats.minifiedSize}`);
+	}))
+	.pipe(gulp.dest('dist/css'))
+	.pipe(notify({ message: 'CSS task complete' }))
+);
+
+gulp.task('cleanCss', () =>
+	gulp.src(['dist/css'], { read: false })
+	.pipe(clean())
+);
+
+
+
+gulp.task('images', ['cleanImages'], () =>
+	gulp.src(imageFiles)
+	.pipe(image())
+	.pipe(gulp.dest('dist/img'))
+	.pipe(notify({ message: 'Images task complete' }))
+);
+
+gulp.task('cleanImages', () =>
+  gulp.src(['dist/img'], { read: false })
+	.pipe(clean())
+);
+
+
+

145c4aec54b1579e141b82c5f77bb074a7668478
Cody Marx Bailey <superphly@gmail.com>
2017-02-08 14:58:23 -0700

    no message

--- partials/mod-now.php
+++ partials/mod-now.php
@@ -11,28 +11,28 @@
 				<div class="row">
 					<div class="col-xs-12 content">
 						<h2>Upcoming BILs</h2>
-					</div>
-					<?php while ( $future_bils->have_posts() ) : $future_bils->the_post(); ?>
-						<div class="col-xs-12 col-sm-4 content">
-							<h2><?php the_title(); ?></h2>
-							<p>
-								<?php the_field('general_location'); ?><br>
-								<?php if (get_field('start_date')) { 
-									$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
-									echo $start_date->format('F jS, Y');
-								} ?>
-							
-								<?php if (get_field('end_date')) {
-									$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
+						<?php while ( $future_bils->have_posts() ) : $future_bils->the_post(); ?>
+							<div class="col-xs-12 col-sm-4 content">
+								<h2><?php the_title(); ?></h2>
+								<p>
+									<?php the_field('general_location'); ?><br>
+									<?php if (get_field('start_date')) { 
+										$start_date = DateTime::createFromFormat('Ymd', get_field('start_date'));
+										echo $start_date->format('F jS, Y');
+									} ?>
+								
+									<?php if (get_field('end_date')) {
+										$end_date = DateTime::createFromFormat('Ymd', get_field('end_date'));
 
-									if ($end_date != $start_date) { // Case for one day event(s)
-										echo '&nbsp;-&nbsp;';
-										echo $end_date->format('F jS, Y');
-									}
-								} ?>
-							</p>
-						</div>
-					<?php endwhile; ?>
+										if ($end_date != $start_date) { // Case for one day event(s)
+											echo '&nbsp;-&nbsp;';
+											echo $end_date->format('F jS, Y');
+										}
+									} ?>
+								</p>
+							</div>
+						<?php endwhile; ?>
+					</div>
 				</div>
 			</div>
 		</div>
